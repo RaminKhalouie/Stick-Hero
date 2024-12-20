4f08f27322d033d13c8dc5978d7bd5937b43a8a9
Ramin Khalouie <ra.khalouie@gmail.com>
2024-12-20 16:15:00 +0330

    [FEAT] : Complete game logic
    Complete game logic
    Add all functionallity
    Add Play / Reset
    etc ...

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,170 @@
 // Select Elements
 const main = document.querySelector('main');
+const logo = document.getElementById('logo');
+const score = document.getElementById('score');
+const playBtn = document.getElementById('play');
+const gameover = document.getElementById('gameover');
+const resetBtn = document.getElementById('reset');
+const ground = document.getElementById('ground');
+const hero = document.getElementById('hero');
+
+let isPlaying = false;
+let linesCount = 0;
+let activeLine = 1;
+let playerScore = 0;
 
 setBackGroundImage();
+playBtn.addEventListener('click', play);
 
 // Functions
 function setBackGroundImage() {
     const number = Math.floor(Math.random() * 10) + 1;
     const imgClass = 'bg-img-' + number;
     main.classList.add(imgClass);
-    console.log(imgClass);
+    generateLine();
 
+    const heroLeft = calcLeft(ground.children[activeLine]);
+    hero.style.left = heroLeft + "px";
+    hero.dataset.hr = heroLeft;
+    ground.dataset.l = ground.offsetLeft;
+}
+
+ground.dataset.l2 = ground.offsetLeft;
+
+function calcLeft(el) {
+    return el.offsetLeft + (el.clientWidth / 2);
+}
+
+function play() {
+    logo.classList.add('hidden');
+    playBtn.classList.add('hidden');
+    resetBtn.classList.add('hidden');
+    gameover.classList.add('hidden');
+    score.classList.remove('hidden');
+    isPlaying = true;
+    playerScore = 0;
+    score.textContent = playerScore;
+}
+
+
+function generateLine(count = 20) {
+    for (let i = 0; i < count; i++) {
+        let w = (Math.floor((Math.random() * 8)) + 4) * 10;
+        const m = (Math.floor((Math.random() * 10)) + 4) * 10;
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.width = w + 'px';
+        line.style.marginRight = m + 'px';
+        line.dataset.mr = m;
+
+        const bar = document.createElement('div');
+        bar.classList.add('bar');
+        line.appendChild(bar);
+        ground.appendChild(line);
+        // console.log(w);
+
+    }
+    linesCount += 20;
+}
+
+let inter;
+document.addEventListener('touchstart', strartMove);
+document.addEventListener('touchend', endMove);
+document.addEventListener('mousedown', strartMove);
+document.addEventListener('mouseup', endMove);
+
+function strartMove() {
+    if (isPlaying) {
+        inter = setInterval(() => {
+            const bar = ground.children[activeLine].children[0];
+            const h = bar.clientHeight + 1;
+            bar.style.height = h + 'px';
+        }, 0);
+    }
+}
+
+function endMove() {
+
+    if (isPlaying) {
+        clearInterval(inter);
+        const line = ground.children[activeLine];
+        const bar = line.children[0];
+        bar.classList.add('duration-500');
+        bar.style.transform = 'rotate(90deg)';
+
+        if (
+            bar.clientHeight < (Number(line.dataset.mr) + line.nextElementSibling.clientWidth) &&
+            bar.clientHeight > (Number(line.dataset.mr))) {
+
+            // hero.style.transform = 'translateX(' + (bar.clientHeight + (line.clientWidth - 20)) + 'px)';
+
+
+            const heroLeft = calcLeft(line.nextElementSibling);
+            hero.style.left = heroLeft + "px";
+            hero.dataset.hr = heroLeft;
+            setTimeout(() => {
+                playPass();
+                playerScore += 1;
+                score.textContent = playerScore;
+            }, 300);
+
+
+            ground.dataset.l = (ground.dataset.l * 1) - ((line.dataset.mr * 1) + line.clientWidth);
+            ground.style.left = ground.dataset.l + 'px';
+
+            activeLine += 1;
+            if (activeLine >= linesCount / 2) {
+                generateLine();
+            }
+
+
+        } else {
+            // hero.style.transform = 'translateX(' + (bar.clientHeight + (line.clientWidth - 20)) + 'px)';
+            hero.style.left = ((bar.clientHeight * 1 + hero.dataset.hr * 1) + 20) + 'px';
+            setTimeout(() => {
+                bar.style.transform = 'rotate(90deg) rotate(90deg)';
+            }, 500);
+
+            setTimeout(() => {
+                hero.style.top = '1000px';
+                playDie();
+            }, 700);
+
+            isPlaying = false;
+
+            resetBtn.classList.remove('hidden');
+            gameover.classList.replace('hidden', 'flex');
+
+        }
+
+    }
+}
+
+resetBtn.addEventListener('click', reset);
+
+function reset() {
+
+    while (ground.lastElementChild) {
+        const temp = ground.lastElementChild;
+        if (temp.tagName == 'FIGURE')
+            break;
+        ground.removeChild(temp);
+    }
+    play();
+    activeLine = 1;
+    setBackGroundImage();
+
+    ground.style.left = ground.dataset.l2 + 'px';
+    ground.dataset.l = ground.dataset.l2;
+    hero.style.top = '-58px';
+
+}
+
+function playPass() {
+    const audio = new Audio('assets/audio/pass.wav');
+    audio.play();
+}
+function playDie() {
+    const audio = new Audio('assets/audio/die.wav');
+    audio.play();
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  safelist: ['bg-img-1', 'bg-img-2', 'bg-img-3', 'bg-img-4', 'bg-img-5', 'bg-img-6', 'bg-img-7', 'bg-img-8', 'bg-img-9', 'bg-img-10'],
+  safelist: ['bg-img-1', 'bg-img-2', 'bg-img-3', 'bg-img-4', 'bg-img-5', 'bg-img-6', 'bg-img-7', 'bg-img-8', 'bg-img-9', 'bg-img-10', 'line', 'bar'],
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +15,7 @@ module.exports = {
         "img-8": "url('../images/back/pic-8.png')",
         "img-9": "url('../images/back/pic-9.png')",
         "img-10": "url('../images/back/pic-10.jpg')",
+        "pipe": "url('../images/icon/pipe-green.png')",
       }
     },
   },
