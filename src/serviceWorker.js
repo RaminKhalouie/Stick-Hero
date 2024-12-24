76930a1844c679fa4fda23b184d272de012bdb64
Ramin Khalouie <ra.khalouie@gmail.com>
2024-12-24 19:50:35 +0330

    [ADD] : Add PWA to game
    Add PWA to game

---
+++ src/serviceWorker.js
@@ -0,0 +1,33 @@
+const staticCacheName = "site-static-v1";
+const cacheAssets = [
+    "/Stick-Hero/src/index.html",
+    "/Stick-Hero/src/",
+    "/Stick-Hero/src/assets/",
+];
+
+self.addEventListener("install", evt => {
+    evt.waitUntil(
+        caches
+            .open(staticCacheName)
+            .then(cache => {
+                console.log("caching assets...");
+                cache.addAll(cacheAssets);
+            })
+            .catch(err => { })
+    );
+});
+
+self.addEventListener("fetch", evt => {
+    evt.respondWith(
+        caches
+            .match(evt.request)
+            .then(res => {
+                return res || fetch(evt.request);
+            })
+            .catch(err => {
+                if (evt.request.url.indexOf(".html") > -1) {
+                    return caches.match("./index.html");
+                }
+            })
+    );
+});
